c2c2d0ebbadfabf9aecdeb6259352341e76f9fd6
Ahmad Ibrahim <ahmad@MacBook-Pro.hitronhub.home>
2023-10-19 00:47:53 -0700

    Add multiple color schemes

--- lua/amkia/packer.lua
+++ lua/amkia/packer.lua
@@ -14,6 +14,16 @@ return require('packer').startup(function(use)
     }
 
     use { "ellisonleao/gruvbox.nvim" }
+    use { "sainnhe/gruvbox-material" }
+    use { "sainnhe/everforest" }
+    use { "rebelot/kanagawa.nvim" }
+    use { "vv9k/bogster" }
+    use { "srcery-colors/srcery-vim" }
+    use { "NLKNguyen/papercolor-theme" }
+    use { "ajmwagar/vim-deus" }
+    use { "notpratheek/vim-luna" }
+    use { "mbbill/vim-seattle" }
+    use { "baskerville/bubblegum" }
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
@@ -27,20 +37,20 @@ return require('packer').startup(function(use)
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
