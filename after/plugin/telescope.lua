74e0a5f256f31dd5c4a30824b071d832d6db9081
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2023-11-06 13:45:29 -0800

    Switch to using rg for telescope and properly ignore finding .git files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>',
+vim.api.nvim_set_keymap('n', '<Leader>ff',
+    ':lua require("telescope.builtin").find_files({ hidden = true, find_command = { "rg", "--files", "--hidden", "--glob=!.git/*" } })<CR>',
     { noremap = true, silent = true })
 vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
